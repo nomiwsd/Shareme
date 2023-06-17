20151ea40f06d6dcb7cdcd08dc6c24f53b4873a0
nomiwsd <nomimalik8051@gmail.com>
2023-06-18 00:10:36 +0500

    shareme frontend

---
+++ src/components/Feed.jsx
@@ -0,0 +1,49 @@
+import React , { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import Masonarylayout from './Masonarylayout';
+import Spinner from './Spinner';
+
+
+const Feed = () => {
+    const [pins, setPins] = useState();
+    const [loading, setLoading] = useState(false);
+    const { categoryId } = useParams();
+  
+    useEffect(() => {
+      if (categoryId) {
+        setLoading(true);
+        const query = searchQuery(categoryId);
+        client.fetch(query).then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+      } else {
+        setLoading(true);
+  
+        client.fetch(feedQuery).then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+      }
+    }, [categoryId]);
+    const ideaName = categoryId || 'new';
+    if (loading) {
+      return (
+        <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+      );
+    }
+  return (
+    <div>
+        <div>
+      {pins && (
+        <Masonarylayout pins={pins} />
+      )}
+    </div>
+    </div>
+  )
+}
+
+export default Feed
